78c9330f3e37bcf76beb6629cd044b43f8bb5d5e
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2022-12-09 14:15:19 -0500

    Add files via upload

---
+++ Homework/Week_three/3.2_Homework/app.js
@@ -0,0 +1,71 @@
+//Class Cat
+
+// class cat1 {
+//     constructor(name, age, color) {
+//       this.name = name;
+//       this.age = age;
+//       this.color = color;
+//     }    getName(){ 
+//       return this.name
+      
+//      }
+//      getAge(){
+//       return `the age is ${this.age}`
+//      }
+//      getColor(){
+//       return ` favorite color ${this.color}`
+//      }
+    
+//   }
+//   const myCat = new cat1('George', 60, 'black');
+//   const myCatAgain = new cat1('Susan',59,'white');
+
+//   console.log(cat1);
+//   console.log(myCat.getName());
+//   console.log(myCat.getAge());
+//   console.log(myCat.getColor());
+//   console.log(myCatAgain.getName());
+ 
+
+// Class Pirate 
+
+// class Pirate {
+//     constructor(captain, crew, ship) {
+//       this.captain = captain;
+//       this.crew = crew;
+//       this.ship = ship;
+//     }
+//     getName(){
+//       return this.captain
+//     }
+//     getAge(){
+//       return `${this.crew}`
+//     }
+//     getShip(){
+//       return `${this.ship}`
+//     }
+//   }
+
+// const myPirate = new Pirate('Jack', 20, 'blackpearl');
+// const myPirate1 = new Pirate('Mary', 19, 'whitepearl');
+// const myPirate2 = new Pirate('Davy Jones', 58, 'blackpearl');
+// const myPirate3 = new Pirate('Hector Barbosa', 40, 'whitepearl');
+// const myPirate4 = new Pirate('Kevin', 5, 'blackpearl');
+// const myPirate5 = new Pirate('Bob', 12, 'whitepearl');
+// const myPirate6 = new Pirate('Stuart', 7, 'blackpearl');
+
+// const arr1= [myPirate1,myPirate2,myPirate3];
+// const arr2= [myPirate4,myPirate5,myPirate6];
+
+// console.log(myPirate.getName());
+// console.log(myPirate.getAge());
+// console.log(myPirate.getShip());
+ 
+// for (let i = 0; i <arr1.length; i++){
+//   console.log(`The name is ${arr1[i].getName()} the ship name is ${arr1[i].getShip()} the number of the crew members is ${arr1[i].getAge()}`
+//   );
+// }
+// for (let i = 0; i <arr2.length; i++){
+//   console.log(`The name is ${arr2[i].getName()} the ship name is ${arr2[i].getShip()} the number of the crew members is ${arr2[i].getAge()}`
+//   );
+// }
